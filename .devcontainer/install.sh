b08124864fdb4a7c95200cc74f6a40b6388e1079
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2023-04-19 15:54:45 -0700

    Remove dotfile installation script

--- .devcontainer/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Installing dotfiles..."
-git clone -b devcontainer --recurse-submodules https://github.com/mjyc/dotfiles.git ~/dotfiles
-cd ~/dotfiles
-./install-deps.sh
-./sync.sh -f
